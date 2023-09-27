cd64b00bcee0d89c7d1a0e90d293e3d7349bc2d5
Juliusmwash <cutejuliusmwash@gmail.com>
2023-09-27 16:00:15 +0300

    Update variable names

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,8 +1,8 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  const updtPhoto  = uploadPhoto();
-  const crtUser= createUser();
+  const updtPhoto = uploadPhoto();
+  const crtUser = createUser();
   return Promise.all([updtPhoto, crtUser])
     .then((data) => console.log(`${data[0].body} ${data[1].firstName} ${data[1].lastName}`),
       () => console.log('Signup system offline'));
