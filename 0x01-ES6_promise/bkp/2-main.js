febbf3e985189b7724c26463fe74fac78c1b33d0
Juliusmwash <cutejuliusmwash@gmail.com>
2023-09-27 15:46:38 +0300

    Add gitignore file

--- 0x01-ES6_promise/bkp/2-main.js
+++
@@ -1,7 +0,0 @@
-import handleResponseFromAPI from "./2-then";
-
-const promise = Promise.resolve();
-console.log(String(handleResponseFromAPI(promise)));
-
-const promise2 = Promise.resolve();
-console.log(String(handleResponseFromAPI(promise2)));
