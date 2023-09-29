ec6c3815dde0b4c6aa3e4a5d9b03faa625e53aab
Juliusmwash <cutejuliusmwash@gmail.com>
2023-09-29 03:35:59 +0300

    Change some files

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,13 +1,6 @@
 /* eslint-disable no-underscore-dangle */
 export default class Car {
   constructor(brand, motor, color) {
-    if (
-      typeof brand !== 'string'
-      || typeof motor !== 'string'
-      || typeof color !== 'string'
-    ) {
-      throw new TypeError('Attributes must be of type String');
-    }
     this._brand = brand;
     this._motor = motor;
     this._color = color;
