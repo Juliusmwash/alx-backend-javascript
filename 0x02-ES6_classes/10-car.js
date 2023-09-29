02b18cca2bc39f15ad85a02a7a5409ddfe3a12b3
Juliusmwash <cutejuliusmwash@gmail.com>
2023-09-29 03:33:26 +0300

    Add fIle 100-evcar.js

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,6 +1,13 @@
 /* eslint-disable no-underscore-dangle */
 export default class Car {
   constructor(brand, motor, color) {
+    if (
+      typeof brand !== 'string'
+      || typeof motor !== 'string'
+      || typeof color !== 'string'
+    ) {
+      throw new TypeError('Attributes must be of type String');
+    }
     this._brand = brand;
     this._motor = motor;
     this._color = color;
