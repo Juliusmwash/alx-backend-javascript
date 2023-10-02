4877f40bdcc77f81bed37df026e939492b8eb2f6
Juliusmwash <cutejuliusmwash@gmail.com>
2023-10-03 02:47:38 +0300

    Commit files for the fifth task

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+function updateStudentGradeByCity(array, city, newGrades) {
+  const filteredArray = array.filter((obj) => obj.location === city);
+
+  const updatedArray = filteredArray.map((obj) => {
+    const matchingGrade = newGrades.find((gradeObj) => gradeObj.studentId === obj.id);
+    if (matchingGrade) {
+      return { ...obj, grades: matchingGrade.grade };
+    }
+    return { ...obj, grades: 'N/A' };
+  });
+
+  return updatedArray;
+}
+
+module.exports = updateStudentGradeByCity;
