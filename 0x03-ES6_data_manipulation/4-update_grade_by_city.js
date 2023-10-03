56252def2e7230bf38cd2cac4a0e0646d0e620c0
Juliusmwash <cutejuliusmwash@gmail.com>
2023-10-03 04:22:52 +0300

    Fix checker error

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,14 +1,17 @@
-export default function updateStudentGradeByCity(array, city, newGrades) {
-
-  if (!(Array.isArray(array)) ||
-    !(Array.isArray(newGrades))){
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  if (!Array.isArray(students)) {
     return [];
   }
-  return array.filter((obj) => obj.location === city).map((obj) => {
-    const matchingGrade = newGrades.find((gradeObj) => gradeObj.studentId === obj.id);
-    if (matchingGrade) {
-      return { ...obj, grades: matchingGrade.grade };
-    }
-    return { ...obj, grades: 'N/A' };
-  });
+  if (!Array.isArray(newGrades)) {
+    return [];
+  }
+  return students
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const [newGrade] = newGrades.filter((item) => item.studentId === student.id);
+      return {
+        ...student,
+        grade: newGrade ? newGrade.grade : 'N/A',
+      };
+    });
 }
