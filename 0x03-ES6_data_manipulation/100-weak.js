39e7bcfd075d03cee636e67e2b1f1d89a43c29d1
Juliusmwash <cutejuliusmwash@gmail.com>
2023-10-03 03:46:44 +0300

    Commit files for the twelfth task

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const weakMap = new WeakMap();
+
+function queryAPI(endpoint) {
+  if (!weakMap.has(endpoint)) {
+    weakMap.set(endpoint, 0);
+  }
+
+  const queryCount = weakMap.get(endpoint) + 1;
+  weakMap.set(endpoint, queryCount);
+
+  if (queryCount >= 5) {
+    throw new Error('Endpoint load is high');
+  }
+}
+
+export { weakMap, queryAPI };
