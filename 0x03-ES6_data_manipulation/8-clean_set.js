8cb427c9b9adb7e021be4dfb83e590fa42c63952
Juliusmwash <cutejuliusmwash@gmail.com>
2023-10-03 03:25:49 +0300

    Commit files for the nineth task

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+function cleanSet(set, startString) {
+  let result = '';
+  for (const value of set) {
+    if (value.startsWith(startString)
+      && startString !== '') {
+      if (result.length > 0) {
+        result += '-';
+      }
+      result += value.slice(startString.length);
+    }
+  }
+  return result;
+}
+
+export default cleanSet;
