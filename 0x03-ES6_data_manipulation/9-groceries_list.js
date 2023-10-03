73018d8884cab77f77a5ae87ec744d39a98e0629
Juliusmwash <cutejuliusmwash@gmail.com>
2023-10-03 03:30:57 +0300

    Commit files for the tenth task

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+function groceriesList() {
+  const groceryMap = new Map();
+
+  groceryMap.set('Apples', 10);
+  groceryMap.set('Tomatoes', 10);
+  groceryMap.set('Pasta', 1);
+  groceryMap.set('Rice', 1);
+  groceryMap.set('Banana', 5);
+
+  return groceryMap;
+}
+
+export default groceriesList;
