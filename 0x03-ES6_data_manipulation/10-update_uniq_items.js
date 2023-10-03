e86719c5e18b65775d5ae11625300e5f26d96f05
Juliusmwash <cutejuliusmwash@gmail.com>
2023-10-03 03:39:35 +0300

    Commit files for the eleventh task

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+function updateUniqueItems(map) {
+  if (!(map instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+
+  for (const [item, quantity] of map.entries()) {
+    if (quantity === 1) {
+      map.set(item, 100);
+    }
+  }
+}
+
+export default updateUniqueItems;
