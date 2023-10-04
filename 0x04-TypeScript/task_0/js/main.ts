d51b95ed6a90e934b350879ad071ff3cc0e351bb
Juliusmwash <cutejuliusmwash@gmail.com>
2023-10-04 23:55:08 +0300

    Commit files for the Typescript project

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -1,61 +1,39 @@
-/* // Import css file
-import '../styles/styles.css'; */
-
-// Define the Student interface
 interface Student {
-  firstName: string;
-  lastName: string;
-  age: number;
-  location: string;
+	firstName: string;
+	lastName: string;
+	age: number;
+	location: string;
 }
 
-// Create two student objects
-const student1: Student = {
-  firstName: "John",
-  lastName: "Doe",
-  age: 20,
-  location: "New York"
-};
-
-const student2: Student = {
-  firstName: "Jane",
-  lastName: "Smith",
-  age: 22,
-  location: "Los Angeles"
-};
-
-// Create an array named studentsList containing the two students
-const studentsList: Student[] = [student1, student2];
-
-// Function to render the table
-function renderTable(students: Student[]) {
-  const table = document.createElement("table");
-  table.classList.add("student-table"); // Add a CSS class to the table for styling
-  const thead = table.createTHead();
-  const tbody = document.createElement("tbody");
-
-  // Create table header row
-  const headerRow = thead.insertRow();
-  const headerCell1 = document.createElement("th");
-  const headerCell2 = document.createElement("th");
-  headerCell1.textContent = "First Name";
-  headerCell2.textContent = "Location";
-  headerRow.appendChild(headerCell1);
-  headerRow.appendChild(headerCell2);
-
-  students.forEach((student) => {
-    const row = tbody.insertRow();
-    const cell1 = row.insertCell(0);
-    const cell2 = row.insertCell(1);
-
-    cell1.textContent = student.firstName;
-    cell2.textContent = student.location;
-  });
-
-  table.appendChild(thead);
-  table.appendChild(tbody);
-  document.body.appendChild(table);
+const studentOne: Student = {
+	firstName: 'Syd',
+	lastName: 'Barrett',
+	age: 60,
+	location: 'Cambridge'
 }
 
-// Render the table with the student data
-renderTable(studentsList);
+const studentTwo: Student = {
+	firstName: 'Will',
+	lastName: 'Oldham',
+	age: 51,
+	location: 'Louisville'
+}
+
+const studentsList = [studentOne, studentTwo];
+const table: HTMLTableElement = document.createElement('table');
+
+document.body.appendChild(table);
+const head: HTMLTableSectionElement = table.createTHead();
+const row: HTMLTableRowElement = head.insertRow();
+const th1: HTMLTableHeaderCellElement = row.insertCell(0);
+const th2: HTMLTableHeaderCellElement = row.insertCell(1);
+th1.innerHTML = ("<b>First Name</b>");
+th2.innerHTML = ("<b>Location</b>");
+const body: HTMLTableSectionElement = table.createTBody();
+studentsList.map((student) => {
+	const newRow: HTMLTableRowElement = body.insertRow();
+	const firstNameRow: HTMLTableCellElement = newRow.insertCell();
+	const locationRow: HTMLTableCellElement= newRow.insertCell();
+	firstNameRow.innerHTML = student.firstName;
+	locationRow.innerHTML = student.location;
+})
