f9156946234fda1b6fed6f01b32aa97af175e72d
Juliusmwash <cutejuliusmwash@gmail.com>
2023-10-06 17:54:56 +0300

    Solve eslint error

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,11 +1,20 @@
 export default function cleanSet(set, startString) {
+  if (typeof startString !== 'string' || !(set instanceof Set)) return '';
+  return [...set]
+    .filter((el) => el.startsWith(startString))
+    .map((el) => el.slice(startString.length))
+    .join('-');
+}
+
+/* export default function cleanSet(set, startString) {
   if (!startString || typeof startString !== 'string' || typeof set !== 'object') return '';
   return [...set]
     .filter((el) => el.startsWith(startString))
     .map((el) => el.slice(startString.length))
     .join('-');
 }
-/* function cleanSet(set, startString) {
+
+function cleanSet(set, startString) {
   let result = '';
   for (const value of set) {
     if (value.startsWith(startString)
